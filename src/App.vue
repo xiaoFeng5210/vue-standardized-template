419d3553de7e4c8408cf04fe088caed98e6fac3b
Eadweard <aurora.coder.zqf@gmail.com>
2024-10-31 13:35:26 +0800

    vant ts配置

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+function handlerShowToast() {
+  showToast('你好')
+}
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
+  <h1 class="text-3xl font-bold underline" @click="handlerShowToast">
     Hello world!
   </h1>
 </template>
